df9abdbcaefe8403d276bb6d2ac20448ce240c70
Arseni <1919ars@gmail.com>
2021-10-13 12:30:41 +0300

    (pro)

--- env_try_atari.py
+++ env_try_atari.py
@@ -1,6 +1,7 @@
-from pettingzoo.atari import space_invaders_v1
+from pettingzoo.atari import space_invaders_v1, double_dunk_v2
 
-env = env = space_invaders_v1.env()
+env = space_invaders_v1.env()
+env = double_dunk_v2.env()
 
 from pettingzoo.utils import random_demo
 random_demo(env, render=True, episodes=10)
